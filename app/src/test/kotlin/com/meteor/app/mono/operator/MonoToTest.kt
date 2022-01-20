3d1d938aab5c6699909b3b365d9a2d7fc27848d4
unseok.kim <unseok.kim@linecorp.com>
2022-01-20 23:35:19 +0900

    add toFutureTest, Transform

--- app/src/test/kotlin/com/meteor/app/mono/operator/MonoToTest.kt
+++ app/src/test/kotlin/com/meteor/app/mono/operator/MonoToTest.kt
@@ -1,15 +1,52 @@
 package com.meteor.app.mono.operator
 
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import reactor.core.publisher.Mono
+import reactor.core.scheduler.Schedulers
 
 class MonoToTest {
     @Test
     internal fun toFutureTest() {
-        TODO("Not yet implemented")
+        val text = "test"
+        val future = Mono.just(text).toFuture()
+        Assertions.assertThat(future.get()).isEqualTo(text)
     }
 
     @Test
-    internal fun toProcessorTest() {
-        TODO("Not yet implemented")
+    internal fun toFutureDoneTest() {
+        val text = "test"
+        val future = Mono.just(text).toFuture()
+        Assertions.assertThat(future.isDone).isTrue
+        Assertions.assertThat(future.get()).isEqualTo(text)
+    }
+
+    @Test
+    internal fun toFutureThreadCheckTest() {
+        val text = "test"
+        val future = Mono.just(text)
+            .map {
+                Thread.sleep(500)
+                it
+            }
+            .toFuture()
+
+        Assertions.assertThat(future.isDone).isTrue
+        Assertions.assertThat(future.get()).isEqualTo(text)
+    }
+
+    @Test
+    internal fun toFutureAsyncThreadCheckTest() {
+        val text = "test"
+        val future = Mono.just(text)
+            .map {
+                Thread.sleep(500)
+                it
+            }.subscribeOn(Schedulers.boundedElastic())
+            .toFuture()
+
+        Assertions.assertThat(future.isDone).isFalse
+        Assertions.assertThat(future.get()).isEqualTo(text)
+        Assertions.assertThat(future.isDone).isTrue
     }
 }
--- app/src/test/kotlin/com/meteor/app/mono/operator/MonoTransformTest.kt
+++ app/src/test/kotlin/com/meteor/app/mono/operator/MonoTransformTest.kt
@@ -1,15 +1,34 @@
 package com.meteor.app.mono.operator
 
 import org.junit.jupiter.api.Test
+import reactor.core.publisher.Mono
+import reactor.test.StepVerifier
 
 class MonoTransformTest {
     @Test
     internal fun transformTest() {
-        TODO("Not yet implemented")
+        val text = "test"
+        val text2 = "test2"
+        val transformMono = Mono.just(text).transform {
+            //pub->pub
+            it.map { text2 }
+        }
+        StepVerifier.create(transformMono)
+            .expectNext(text2)
+            .verifyComplete()
     }
 
     @Test
     internal fun transformDeferredTest() {
-        TODO("Not yet implemented")
+        val text = "test"
+        val text2 = "test2"
+        val transformMono = Mono.just(text).transformDeferred {
+            //pub->pub
+            it.map { text2 }
+        }
+        StepVerifier.create(transformMono)
+            .expectNext(text2)
+            .verifyComplete()
+
     }
 }
