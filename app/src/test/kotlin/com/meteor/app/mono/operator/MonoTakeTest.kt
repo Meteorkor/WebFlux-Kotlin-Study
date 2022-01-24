5c6db942c9b3856da11c8b34d0434526984fc9ef
unseok.kim <unseok.kim@linecorp.com>
2022-01-24 22:03:52 +0900

    add take, then

--- app/src/test/kotlin/com/meteor/app/mono/operator/MonoTakeTest.kt
+++ app/src/test/kotlin/com/meteor/app/mono/operator/MonoTakeTest.kt
@@ -1,16 +1,78 @@
 package com.meteor.app.mono.operator
 
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import reactor.core.publisher.Mono
+import reactor.test.StepVerifier
+import java.time.Duration
+import java.util.concurrent.atomic.AtomicInteger
 
 class MonoTakeTest {
     @Test
     internal fun takeTest() {
-        TODO("Not yet implemented")
+        val text = "text"
+        val atomicInteger = AtomicInteger(0)
+        val thenMono = Mono.just(text)
+            .map {
+                Thread.sleep(100)
+                it
+            }
+            .take(Duration.ofMillis(1))
+            .map {
+                atomicInteger.incrementAndGet()
+                it
+            }
+
+        StepVerifier.create(thenMono)
+//            .expectNext(text)
+            .verifyComplete()
+
+        Assertions.assertThat(atomicInteger.get()).isEqualTo(0)
     }
 
     @Test
-    internal fun takeUntilOtherTest() {
-        TODO("Not yet implemented")
+    internal fun takeTestNotTimeout() {
+        val text = "text"
+        val atomicInteger = AtomicInteger(0)
+        val thenMono = Mono.just(text)
+            .map {
+                it
+            }
+            .take(Duration.ofMillis(1000))
+            .map {
+                atomicInteger.incrementAndGet()
+                it
+            }
+
+        StepVerifier.create(thenMono)
+            .expectNext(text)
+            .verifyComplete()
+
+        Assertions.assertThat(atomicInteger.get()).isEqualTo(0)
+    }
+
+    @Test
+    internal fun takeUntilOtherTest() {//??
+        val text = "text"
+        val atomicInteger = AtomicInteger(0)
+        val thenMono = Mono.just(text)
+            .map {
+                it
+            }
+            .takeUntilOther {
+                println("asdas")
+            }
+            .map {
+                atomicInteger.incrementAndGet()
+                it
+            }
+
+        StepVerifier.create(thenMono)
+            .expectNext(text)
+//            .expectNext(text)
+            .verifyComplete()
+
+        Assertions.assertThat(atomicInteger.get()).isEqualTo(0)
     }
 
 }
--- app/src/test/kotlin/com/meteor/app/mono/operator/MonoThenTest.kt
+++ app/src/test/kotlin/com/meteor/app/mono/operator/MonoThenTest.kt
@@ -1,25 +1,96 @@
 package com.meteor.app.mono.operator
 
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import reactor.test.StepVerifier
+import java.util.concurrent.atomic.AtomicInteger
 
 class MonoThenTest {
     @Test
+    internal fun thenTestNotSubscribe() {
+        val text = "text"
+        val atomicInteger = AtomicInteger(0)
+        val thenMono = Mono.just(text)
+            .map {
+                atomicInteger.incrementAndGet()
+                it
+            }
+            .then()
+
+        Assertions.assertThat(atomicInteger.get()).isEqualTo(0)
+    }
+
+    @Test
     internal fun thenTest() {
-        TODO("Not yet implemented")
+        val text = "text"
+        val atomicInteger = AtomicInteger(0)
+        val thenMono = Mono.just(text)
+            .map {
+                atomicInteger.incrementAndGet()
+                it
+            }
+            .then()
+
+        StepVerifier.create(thenMono)
+            .verifyComplete()
+
+        Assertions.assertThat(atomicInteger.get()).isEqualTo(1)
     }
 
     @Test
     internal fun thenReturnTest() {
-        TODO("Not yet implemented")
+        val text = "text"
+        val atomicInteger = AtomicInteger(0)
+        val thenMono = Mono.just(text)
+            .map {
+                atomicInteger.incrementAndGet()
+                it + "!!!"
+            }
+            .thenReturn(text)
+
+        StepVerifier.create(thenMono)
+            .expectNext(text)
+            .verifyComplete()
+
+        Assertions.assertThat(atomicInteger.get()).isEqualTo(1)
     }
 
     @Test
     internal fun thenEmptyTest() {
-        TODO("Not yet implemented")
+        val text = "text"
+        val atomicInteger = AtomicInteger(0)
+        val thenMono = Mono.just(text)
+            .map {
+                atomicInteger.incrementAndGet()
+                it + "!!!"
+            }
+            .thenEmpty {
+                it.onComplete()
+            }
+
+        StepVerifier.create(thenMono)
+            .verifyComplete()
+
+        Assertions.assertThat(atomicInteger.get()).isEqualTo(1)
     }
 
     @Test
     internal fun thenManyTest() {
-        TODO("Not yet implemented")
+        val text = "text"
+        val atomicInteger = AtomicInteger(0)
+        val thenMono = Mono.just(text)
+            .map {
+                atomicInteger.incrementAndGet()
+                it + "!!!"
+            }
+            .thenMany(Flux.just("a", "b"))
+
+        StepVerifier.create(thenMono)
+            .expectNext("a", "b")
+            .verifyComplete()
+
+        Assertions.assertThat(atomicInteger.get()).isEqualTo(1)
     }
 }
