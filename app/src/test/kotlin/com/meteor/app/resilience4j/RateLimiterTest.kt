5d59c5e83d7218002c819c7ba03effc053d5b746
unseok.kim <unseok.kim@linecorp.com>
2022-08-11 08:27:30 +0900

    add rateLimiterMillisTest

--- app/src/test/kotlin/com/meteor/app/resilience4j/RateLimiterTest.kt
+++ app/src/test/kotlin/com/meteor/app/resilience4j/RateLimiterTest.kt
@@ -41,4 +41,38 @@ class RateLimiterTest {
             }
     }
 
+
+    @Test
+    internal fun rateLimiterMillisTest() {
+        val tpsPerServer = 1
+        val timeDuration = 1L
+
+        val rateLimiter = RateLimiter.of(
+            "tempLimiter", RateLimiterConfig.custom()
+                .limitRefreshPeriod(
+                    Duration.ofMillis(100)
+                ).limitForPeriod(tpsPerServer)
+                .build()
+        )
+
+
+        val permits = 1
+        rateLimiter.acquirePermission(permits)
+        IntStream.range(0, 10)
+            .forEach {
+                val sw = StopWatch()
+                sw.start()
+                //1tps로 제한되어서 확보될때까지 대기
+                rateLimiter.acquirePermission(permits)
+                sw.stop()
+
+                println("sw.totalTimeMillis : ${sw.totalTimeMillis}")
+
+
+                //mill 는 더 부정확해짐
+                Assertions.assertTrue(sw.totalTimeMillis >= (permits * 100) * 0.9)
+                Assertions.assertTrue(sw.totalTimeMillis <= ((permits * 100) + 1))
+            }
+    }
+
 }
